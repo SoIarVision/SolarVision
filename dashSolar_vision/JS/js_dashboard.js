bf8334d0faa0b0a3725548d380a3ee63847c87e5
Pedro-Caparroz <pedrohb.caparroz@gmail.com>
2026-04-12 14:30:32 -0300

    Dashboard projeto inicial

--- dashSolar_vision/JS/js_dashboard.js
+++ dashSolar_vision/JS/js_dashboard.js
@@ -22,7 +22,13 @@ new Chart(ctx, {
               data: [75, 75 ,75, 75, 75, 75, 75, 75, 75, 75,75],
               type: 'line',
               backgroundColor: 'rgb(225,0,0)',
-              order: 1
+              borderColor: 'red',
+              order: 1, 
+              borderWidth: 2,
+              borderHeight: 5,
+              pointRadius: 0,
+
+              
           }
 
         ]
@@ -30,10 +36,18 @@ new Chart(ctx, {
     options: {
         responsive: true,
         plugins: {
-            title: {
+            title :  {
             display: true,
-            text: 'Eficiencia da placa solar e perda por soiling',
-        }
+            text : 'Perda por Soiling Mensal ',     
+              font: {
+                size: 20,
+                weigth: 'bold'
+            },
+            padding: {
+                bottom: 30,
+            },
+           }, 
+        legend: { position: 'bottom'}
     },
         scales: {
             y: {
@@ -47,6 +61,7 @@ new Chart(ctx, {
             x: {
                 type: 'bar',
                 stacked: true,
+                offset: false,
                 title: {
                     display: true,
                     text: 'Meses'
@@ -55,3 +70,81 @@ new Chart(ctx, {
         }
     }
 );
+
+const ctx_linha = document.getElementById("eficiencia_por_dia");
+
+/* DECLARAÇÃO DE VARIÁVEIS PARA MOCKAR DADOS */ 
+const dias = [];
+for(let i = 1; i <=30 ; i++){
+   dias.push(i);
+}
+
+const dados = [];
+let valores = 100
+for(let i = 1; i <=30 ; i++){
+   dados.push(valores);
+
+   valores -= 0.5
+}
+
+/* dados[4] = 50 */
+
+
+/* CRIAÇÃO E CONFIGURAÇÃO DOS GRÁFICOS */
+
+new Chart(ctx_linha, {
+    type: 'line',
+    data: {
+        labels: dias,
+        datasets: [{
+        
+        label:  'Eficiência diária',
+        data: dados,
+
+        }
+         
+        ] 
+
+    },
+
+    options: {
+        responsive: true,
+        plugins: {
+            title: {
+            display: true,
+            text: 'Eficiência da placa solar ',
+            font: {
+                size: 20,
+                weigth: 'bold'
+            },
+            padding: {
+                bottom: 30,
+            }
+        },
+        legend: { position: 'bottom'}
+    },
+        scales: {
+            y: {
+                type: 'linear',
+                position: 'left',
+                stacked: true,
+                min: 0,
+                max: 100,
+                }
+            },
+            
+            x: {
+                type: 'line',
+                title: {
+                    display: true,
+                    text: 'dias'
+                }
+                
+            }
+        }
+}
+
+
+);
+
+/* verificações de variável */
