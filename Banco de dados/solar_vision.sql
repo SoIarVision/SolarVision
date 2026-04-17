c7638ff5afed6384dcaa078042d39457dd92ecbc
EmillyMaffei <emilly.oliveira@sptech.school>
2026-04-17 15:18:29 -0300

    Banco de dados com insert e select atualizados

--- Banco de dados/solar_vision.sql
+++ Banco de dados/solar_vision.sql
@@ -1,25 +1,40 @@
+drop database solar_vision;
 CREATE DATABASE solar_vision;
 USE solar_vision;
+
+
+create table contato ( 
+idcontato int primary key auto_increment, 
+email varchar (100), 
+telprin varchar (11), 
+telreserva varchar (13));
+create table endereco (
+idendereco int primary key auto_increment, 
+cep char (8) , 
+rua varchar(100), 
+bairro varchar (100),
+estado varchar (100),
+complemento varchar(1000));
 
 CREATE TABLE empresa( -- empresa
 	id_empresa INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100),
-    cnpj CHAR(11) UNIQUE,
-    contato VARCHAR(50),
+    cnpj CHAR(14) UNIQUE,
     email varchar(80),
-    endereco varchar (80)
+    fkendereco int , 
+    constraint fkdoendereco foreign key (fkendereco) references endereco(idendereco)
 );
 
 create table usuario( 
 id_usuario int auto_increment primary key , 
 nome varchar (60) not null , 
 cpf char (11) unique,
-contato varchar (15),
-email varchar (60) not null ,
 fk_empresa int , -- se é um funcionario responsavel de tal empresa
 constraint fk_empresa_resp foreign key (fk_empresa) references empresa(id_empresa),
 senha varchar (50) not null,
-permissao tinyint default(0) -- se for 0 responsavel , se for 1 adm
+permissao tinyint default(0),-- se for 0 responsavel , se for 1 adm
+ fkcontato int, 
+    constraint fkcontatousuario foreign key (fkcontato) references contato(idcontato)
 );
 
 
@@ -48,21 +63,31 @@ CREATE TABLE registro ( -- medida
     constraint arduinofk foreign key (fkarduino) references sensores(id_grupo)
 )auto_increment = 0;
 
-insert into empresa (nome,cnpj,contato,email,endereco) values 
-('BYD ENERGY','04567898765','11967054392','bydenergy@gmail.com','Rua Magalhaes 350'),
-('Complexo Janaúva','10454392321','11970654921','complexojanauva@gmail.com','Rua Manoel Jardim 3456 '),
-('Samsung','23403291232','11979065932','samsungcontato@gmail.com',NULL);
 
+ insert into endereco(cep,rua,bairro,estado,complemento) values 
+ ('12345869','Luis Alexandre','Olimpico','SP','acima do muro'),
+ ('10459345','Marques de Andrade','Fernandes','SP',null),
+ ('45034569','Martinelli Jaos','Mokfg','RJ','Prédio Roxo');
+
+ select * from usuario ;
  
- 
-insert into usuario (nome,cpf,contato,email,fk_empresa,senha) values 
-('Leonardo Pires','67044302901','11989456032','leonardoresp@gmail.com',1,'leopiresmk'),
-('Manoela Albuquerque','23400192454',null,'manoelaalb@gmail.com',2,'manoelaalbufd');
+ insert into empresa (nome,cnpj,fkendereco) values 
+ ('SAMSUNG','123694549650',1) ,
+ ('APPLE','104569394569',2);
+ insert into contato (email,telprin) values 
+ ('leonardoresp@gmail.com','11989456032'),
+ ('manoelaalb@gmail.com','11945382453');
+
+insert into usuario (nome,cpf,fk_empresa,senha,fkcontato) values 
+('Leonardo Pires','67044302901',1,'leopiresmk',1),
+('Manoela Albuquerque','23400192454',2,'manoelaalbufd',2);
 
 insert into placa (fk_empresa) values 
 (1),
 (2);
-
+select * from placa;
+update placa set localizacao = '3BA' where id_placa = 1;
+update placa set localizacao = 'MSD' where id_placa = 2;
 insert into sensores (tipo,fk_placa) values 
 ('Controle',1),
 ('Controle',1),
@@ -71,18 +96,26 @@ insert into sensores (tipo,fk_placa) values
 ('Ideal',1),
 ('Ideal',1);
 
+select * from sensores;
 
 insert into registro (valorLuminosidade, fkarduino) values 
 (700, 1),
-(800, 1);
+(800, 2),
+(700,3),
+(860,4),
+(500,5),
+(780,6);
 
-
+-- Registros e as empresas 
 select e.nome as empresa , u.nome 'Funcionário Responsável' , p.localizacao as 'Localizacao da Placa' , s.tipo as 'Tipo de Dado' , 
 r.valorLuminosidade as 'Registro' from empresa as e 
 join usuario as u on u.fk_empresa = e.id_empresa 
 join placa as p  on p.fk_empresa = e.id_empresa 
 join sensores as s on s.fk_placa = p.id_placa 
-join registro as r on r.fkarduino = s.id_grupo ; 
+join registro as r on r.fkarduino = s.id_grupo; 
 
-select e.nome as 'Empresa', u.nome 'Funcionário Responsável', u.email from empresa as e join usuario as u on fk_empresa = id_empresa
-where fk_empresa >= 0;
+
+-- Quais os funcionarios responsáveis das empresas e o email deles
+select e.nome as 'Empresa', u.nome 'Funcionário Responsável',c.email from empresa as e
+ join usuario as u on fk_empresa = id_empresa
+ join contato as c on c.idcontato = u.fkcontato;
