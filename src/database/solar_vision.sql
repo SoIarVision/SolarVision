86eff9f74a6f57259e648b897517fb4bc7ab2107
Lucas-Galvao07 <lucas.galvao@sptech.school>
2026-05-28 15:07:12 -0300

    Refinando banco de dados

--- src/database/solar_vision.sql
+++ src/database/solar_vision.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS solar_Vision;
 CREATE DATABASE IF NOT EXISTS solar_Vision;
 USE solar_Vision;
 
@@ -5,8 +6,6 @@ CREATE TABLE empresa (
 idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(100) NOT NULL,
 cnpj CHAR(14) UNIQUE NOT NULL,
-contato VARCHAR(50),
-email VARCHAR(80),
 endereco VARCHAR(100)
 );
 
@@ -39,37 +38,36 @@ CONSTRAINT fkPlacaEmpresa FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
 CREATE TABLE grupo_sensor (
 idSensor INT PRIMARY KEY AUTO_INCREMENT,
 localizacao VARCHAR(45),
-tipo CHAR(8),
+tipo VARCHAR(8),
 CONSTRAINT chkTipoSensor CHECK (tipo IN ('Controle', 'Ideal')),
 status_sensor VARCHAR(50),
-valor_leitura VARCHAR(40),
+valor_leitura INT,
 fkPlaca INT,
-fkEmpresa INT,
-CONSTRAINT fkSensorPlaca FOREIGN KEY (fkPlaca)REFERENCES placa(idPlaca),
-CONSTRAINT fkSensorEmpresa FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
+CONSTRAINT fkSensorPlaca FOREIGN KEY (fkPlaca)REFERENCES placa(idPlaca)
 );
 
 CREATE TABLE registro (
-idRegistro INT PRIMARY KEY AUTO_INCREMENT,
+idRegistro INT AUTO_INCREMENT,
 valor INT,
 data_registro DATETIME DEFAULT CURRENT_TIMESTAMP,
 fkSensor INT,
+PRIMARY KEY (idRegistro, fkSensor),
 CONSTRAINT fkRegistroSensor FOREIGN KEY (fkSensor) REFERENCES grupo_sensor(idSensor)
 );
 
-CREATE TABLE eficiencia (
-idEficiencia INT PRIMARY KEY AUTO_INCREMENT,
-valor_eficiencia VARCHAR(45),
+CREATE TABLE historico_eficiencia (
+idEficiencia INT AUTO_INCREMENT,
+valor_eficiencia DECIMAL(4,1),
+dt_eficiencia datetime default current_timestamp,
 fkPlaca INT,
-fkEmpresa INT,
-CONSTRAINT fkEficienciaPlaca FOREIGN KEY (fkPlaca) REFERENCES placa(idPlaca),
-CONSTRAINT fkEficienciaEmpresa FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
+PRIMARY KEY (idEficiencia, fkPlaca),
+CONSTRAINT fkEficienciaPlaca FOREIGN KEY (fkPlaca) REFERENCES placa(idPlaca)
 );
 
-INSERT INTO empresa (nome, cnpj, contato, email, endereco) VALUES
-('BYD ENERGY', '04567898765432', '11967054392', 'bydenergy@gmail.com', 'Rua Magalhaes 350'),
-('Samsung', '23403291232123', '11979065932', 'samsungcontato@gmail.com', 'Av Paulista 1000'),
-('Complexo Janaúva', '10454392321456', '11970654921', 'complexojanauva@gmail.com', 'Rua Manoel Jardim 3456');
+INSERT INTO empresa (nome, cnpj, endereco) VALUES
+('BYD ENERGY', '04567898765432', 'Rua Magalhaes 350'),
+('Samsung', '23403291232123', 'Av Paulista 1000'),
+('Complexo Janaúva', '10454392321456', 'Rua Manoel Jardim 3456');
 
 INSERT INTO cargo (cargo) VALUES
 ('Administrador'),
@@ -79,27 +77,26 @@ INSERT INTO cargo (cargo) VALUES
 
 INSERT INTO usuario (nome, cpf, contato, email, senha, fkCargo, fkEmpresa) VALUES
 ('Leonardo Pires', '67044302901', '11989456032', 'leonardoresp@gmail.com', 'leopiresmk', 3, 1),
-('Manoela Albuquerque', '23400192454', '11988887777', 'manoelaalb@gmail.com', 'manoelaalbufd', 2, 2);
+('Manoela Albuquerque', '23400192454', '11988887777', 'manoelaalb@gmail.com', 'manoelaalbufd', 2, 2),
+('Lucas', '77777777777','','lucass3neogalvao@gmail.com','lucas12#',1,null);
 
 INSERT INTO placa (fkEmpresa, localizacao, descricao) VALUES
 (1, 'Setor Norte', 'Placa principal da BYD'),
 (2, 'Setor Sul', 'Placa secundária da Samsung');
 
-INSERT INTO grupo_sensor 
-(local, tipo, status_sensor, valor_leitura, fkPlaca, fkEmpresa)
-VALUES
-('Painel A1', 'Controle', 'Ativo', '750', 1, 1),
-('Painel A2', 'Ideal', 'Ativo', '820', 1, 1),
-('Painel B1', 'Controle', 'Manutenção', '610', 2, 2);
+INSERT INTO grupo_sensor (localizacao, tipo, status_sensor, valor_leitura, fkPlaca) VALUES
+('Painel A1', 'Controle', 'Ativo', '750', 1),
+('Painel A2', 'Ideal', 'Ativo', '820', 1),
+('Painel B1', 'Controle', 'Manutenção', '610', 2);
 
 INSERT INTO registro (valor, fkSensor) VALUES
 (750, 1),
 (820, 2),
 (610, 3);
 
-INSERT INTO eficiencia (valor_eficiencia, fkPlaca, fkEmpresa) VALUES
-('92%', 1, 1),
-('87%', 2, 2);
+INSERT INTO historico_eficiencia (valor_eficiencia, fkPlaca) VALUES
+(92.4, 1),
+(87.5, 2);
 
 CREATE VIEW view_detalhamento_sensor AS
 SELECT
@@ -117,7 +114,7 @@ JOIN empresa e
 ON p.fkEmpresa = e.idEmpresa
 JOIN usuario u
 ON u.fkEmpresa = e.idEmpresa
-LEFT JOIN eficiencia ef
+LEFT JOIN historico_eficiencia ef
 ON ef.fkPlaca = p.idPlaca
 JOIN grupo_sensor g
 ON g.fkPlaca = p.idPlaca;
@@ -166,3 +163,18 @@ JOIN placa p
 ON g.fkPlaca = p.idPlaca
 JOIN empresa e
 ON p.fkEmpresa = e.idEmpresa;
+
+select * from view_detalhamento_sensor;
+select * from view_registro_ref_empresa;
+select * from view_funcionario;
+select * from view_registro;
+
+SELECT 
+	u.idUsuario id, 
+    u.nome,
+    u.email, 
+    e.nome as empresa,
+    c.cargo cargo
+FROM usuario u
+LEFT JOIN empresa e ON u.fkEmpresa = e.idEmpresa
+LEFT JOIN cargo c ON u.fkCargo = c.idCargo;
--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -3,7 +3,15 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT idusuario, nome, email, fkempresa as empresaId FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+    SELECT 
+	    u.idUsuario id, 
+        u.nome,
+        u.email, 
+        e.nome as empresa,
+        c.cargo cargo
+    FROM usuario u
+    LEFT JOIN empresa e ON u.fkEmpresa = e.idEmpresa
+    LEFT JOIN cargo c ON u.fkCargo = c.idCargo WHERE email = '${email}' AND senha = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -12,7 +20,7 @@ function autenticar(email, senha) {
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
 function cadastrar(nome, email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
-    
+
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
